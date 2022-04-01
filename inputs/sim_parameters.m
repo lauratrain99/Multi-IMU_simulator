9330cc9b6263c629b50c0d5f09734857ac65de90
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2022-04-01 14:08:12 +0200

    attitude estimation no syntax errors

--- inputs/sim_parameters.m
+++ inputs/sim_parameters.m
@@ -18,17 +18,20 @@ save('config.mat','config','-v7.3');
 freq = 200;
 Ts = 1/freq;
 
-imu1.gyro.bias = (([6.1, 9.22, 2.89]*1e-5)*180/pi).^2/(2*pi); %(deg/s)^2
+imu1.gyro.bias = ([6.1, 9.22, 2.89]*1e-5)*180/pi; %deg/s
 imu1.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
 imu1.alignment = [0; 0; 0];
 imu1.gyro.fullrange = 2000;
 imu1.gyro.quantization = 2*imu1.gyro.fullrange/2^16;
+imu1.gyro.std = sqrt(imu1.gyro.noisepower*freq);
 imu1.acc.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
 imu1.acc.fullrange = 16*9.8; %m/s^2
 imu1.acc.quantization = 2*imu1.acc.fullrange/2^16;
+imu1.acc.std = sqrt(imu1.acc.noisepower*freq);
 imu1.mag.fullrange = 4800; %microT
 imu1.mag.quantization = 2*imu1.mag.fullrange/2^14;
 imu1.mag.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
+imu1.mag.std = sqrt(imu1.mag.noisepower*freq);
 
 imu2.gyro.bias = (([6.1, 9.22, 2.89]*1e-5)*180/pi).^2/(2*pi); %(deg/s)^2
 imu2.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
@@ -66,11 +69,8 @@ imu4.mag.fullrange = 4800; %microT
 imu4.mag.quantization = 2*imu4.mag.fullrange/2^14;
 imu4.mag.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
 
-noiseMag = ((50e-3)^2); %microT^2/Hz
+%Kalman initialization
 
-config.imu2_alignment = [0; 0; 0];
-config.imu3_alignment = [0; 0; 0];
-config.imu3_alignment = [0; 0; 0];
-
+% gyro_bias_init = [sqrt(biasAng);sqrt(biasAng);sqrt(biasAng) ]*100;
 
 % set_param(gcs,'EnableLBRepository','on');
