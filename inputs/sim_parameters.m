54a6161391b8f2b4236d8430bb4140b6834f2496
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2022-04-23 13:31:20 +0200

    1 imu kf working

--- inputs/sim_parameters.m
+++ inputs/sim_parameters.m
@@ -18,7 +18,8 @@ save('config.mat','config','-v7.3');
 freq = 200;
 Ts = 1/freq;
 
-imu1.gyro.bias = ([6, 6, 6]*1e-5)*180/pi; %deg/s
+% Sensor specifications
+imu1.gyro.bias = ([6; 6; 6]*1e-5)*180/pi; %deg/s
 imu1.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
 imu1.alignment = [0; 0; 0];
 imu1.gyro.fullrange = 2000; 
@@ -38,39 +39,45 @@ imu2.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
 imu2.alignment = [0; 0; 0];
 imu2.gyro.fullrange = 2000;
 imu2.gyro.quantization = 2*imu2.gyro.fullrange/2^16;
+imu2.gyro.std = sqrt(imu2.gyro.noisepower*freq*pi/180); %rad/s
 imu2.acc.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
 imu2.acc.fullrange = 16*9.8; %m/s^2
 imu2.acc.quantization = 2*imu2.acc.fullrange/2^16;
+imu2.acc.std = sqrt(imu2.acc.noisepower*freq);
 imu2.mag.fullrange = 4800; %microT
 imu2.mag.quantization = 2*imu2.mag.fullrange/2^14;
 imu2.mag.noisepower = ([50, 50, 50]*1e-3).^2; %(microT)^2/Hz
+imu2.mag.std = sqrt(imu2.mag.noisepower*freq);
 
 imu3.gyro.bias = (([6.1, 9.22, 2.89]*1e-5)*180/pi).^2/(2*pi); %(deg/s)^2
 imu3.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
 imu3.alignment = [0; 0; 0];
 imu3.gyro.fullrange = 2000;
 imu3.gyro.quantization = 2*imu3.gyro.fullrange/2^16;
+imu3.gyro.std = sqrt(imu3.gyro.noisepower*freq*pi/180); %rad/s
 imu3.acc.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
 imu3.acc.fullrange = 16*9.8; %m/s^2
 imu3.acc.quantization = 2*imu3.acc.fullrange/2^16;
+imu3.acc.std = sqrt(imu3.acc.noisepower*freq);
 imu3.mag.fullrange = 4800; %microT
 imu3.mag.quantization = 2*imu3.mag.fullrange/2^14;
 imu3.mag.noisepower = ([50, 50, 50]*1e-3).^2; %(microT)^2/Hz
+imu3.mag.std = sqrt(imu3.mag.noisepower*freq);
 
 imu4.gyro.bias = (([6.1, 9.22, 2.89]*1e-5)*180/pi).^2/(2*pi); %(deg/s)^2
 imu4.gyro.noisepower = [0.01, 0.01, 0.01].^2; %(deg/s)^2/Hz
 imu4.alignment = [0; 0; 0];
 imu4.gyro.fullrange = 2000;
 imu4.gyro.quantization = 2*imu4.gyro.fullrange/2^16;
+imu4.gyro.std = sqrt(imu4.gyro.noisepower*freq*pi/180); %rad/s
 imu4.acc.noisepower = ([300, 300, 300]*1e-6*9.8).^2; %(m/s^2)^2/Hz
 imu4.acc.fullrange = 16*9.8; %m/s^2
 imu4.acc.quantization = 2*imu4.acc.fullrange/2^16;
+imu4.acc.std = sqrt(imu4.acc.noisepower*freq);
 imu4.mag.fullrange = 4800; %microT
 imu4.mag.quantization = 2*imu4.mag.fullrange/2^14;
 imu4.mag.noisepower = ([50, 50, 50]*1e-3).^2; %(microT)^2/Hz
+imu4.mag.std = sqrt(imu4.mag.noisepower*freq);
 
-%Kalman initialization
-
-% gyro_bias_init = [sqrt(biasAng);sqrt(biasAng);sqrt(biasAng) ]*100;
 
 % set_param(gcs,'EnableLBRepository','on');
